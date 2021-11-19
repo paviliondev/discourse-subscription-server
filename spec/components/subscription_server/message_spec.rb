57f5d33a7d73e9f5beaef3f1ff2e49870da48e22
angusmcleod <angus@mcleod.org.au>
2021-11-19 12:12:56 +0800

    Fix failing specs

--- spec/components/subscription_server/message_spec.rb
+++ spec/components/subscription_server/message_spec.rb
@@ -3,23 +3,23 @@ require_relative '../../plugin_helper'
 
 describe SubscriptionServer::Message do
   it "#create" do
-    message = described_class.create(message: "Info message")
+    message = described_class.create(title: "Info message")
     expect(message.class).to eq(described_class)
   end
 
   it "#create different types" do
-    message = described_class.create(message: "Warning message", type: described_class.types[:warning])
+    message = described_class.create(title: "Warning message", type: described_class.types[:warning])
     expect(message.type).to eq(described_class.types[:warning])
   end
 
   it "#find" do
-    message = described_class.create(message: "Info message")
+    message = described_class.create(title: "Info message")
     expect(described_class.find(message.id).message).to eq(message.message)
   end
 
   it "#list" do
-    described_class.create(message: "Info message")
-    described_class.create(message: "Warning message", type: described_class.types[:warning])
+    described_class.create(title: "Info message")
+    described_class.create(title: "Warning message", type: described_class.types[:warning])
     expect(described_class.list.length).to eq(2)
   end
 end
