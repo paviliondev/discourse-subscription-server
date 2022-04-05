2a31a0fab47060acb81265e65e900c9558523327
Angus McLeod <angus@mcleod.org.au>
2022-04-05 16:21:02 +0200

    Revert "Remove unnecessary code for messages"
    This reverts commit b49a0d5b340ca0071a984391bbfba3d7ba3f425f.

--- lib/subscription_server/message.rb
+++ lib/subscription_server/message.rb
@@ -76,9 +76,14 @@ class SubscriptionServer::Message
     end
   end
 
-  def self.list
-    PluginStoreRow.where(plugin_name: namespace)
-      .order("value::json->>'created_at' DESC")
+  def self.list_query(attr = nil, value = nil)
+    query = PluginStoreRow.where(plugin_name: namespace)
+    query = query.where("(value::json->>'#{attr}') = ?", value) if attr && value
+    query.order("value::json->>'created_at' DESC")
+  end
+
+  def self.list(attr = nil, value = nil)
+    list_query(attr, value)
       .map { |r| new(r.key, JSON.parse(r.value).symbolize_keys) }
   end
 end
